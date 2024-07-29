72972207fc97687fb556c60a89dd70376ab8b145
Jerold <jjjjjssss0326@gmail.com>
2024-07-30 01:22:00 +1000

    fixed chromedriver local installation

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 def main():
     # Install chrome driver in current directory
-    os.environ['WEB_DRIVER_MANAGER_CACHE_DIR'] = os.getcwd()
+    os.environ['WDM_LOCAL'] = '1'
     ChromeDriverManager().install()
 
+if __name__ == '__main__':
+    main() 
