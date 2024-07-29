b69bb476ce262f4a53eba9c87e69d88555f2d261
Jerold <jjjjjssss0326@gmail.com>
2024-07-30 02:47:20 +1000

    fixed getting chromedriver directory by giving it as argument to scraping functions

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import os
 from webdriver_manager.chrome import ChromeDriverManager
+from pinnacle import get_pin_odds
 
 def main():
     # Install chrome driver in current directory
     os.environ['WDM_LOCAL'] = '1'
-    ChromeDriverManager().install()
+    driver_path = ChromeDriverManager().install()
+
+    get_pin_odds(driver_path)
 
 if __name__ == '__main__':
     main() 
