cc39dcd655ce5bcfb938f4d7778cb8f885513bdc
Jerold <jjjjjssss0326@gmail.com>
2024-07-30 19:08:11 +1000

    added tab to main

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import os
 from webdriver_manager.chrome import ChromeDriverManager
 from pinnacle import get_pin_odds
+from tab import get_tab_odds
 
 def main():
     # Install chrome driver in current directory
     os.environ['WDM_LOCAL'] = '1'
     driver_path = ChromeDriverManager().install()
 
-    get_pin_odds(driver_path)
+    #get_pin_odds(driver_path)
+    print(get_tab_odds(driver_path))
 
 if __name__ == '__main__':
     main() 
