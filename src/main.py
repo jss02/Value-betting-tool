5b634d607c7429ce1def64b8a1d9bb823a5ffdd2
Jerold <jjjjjssss0326@gmail.com>
2024-08-14 16:06:25 +1000

    Added params to main to optionally disable bookmakers

--- src/main.py
+++ src/main.py
@@ -1,4 +1,3 @@
-import os
 from webdriver_manager.chrome import ChromeDriverManager
 from pinnacle import get_pin_odds
 from tab import get_tab_odds
@@ -8,21 +7,25 @@ from match_events import get_pos_ev
 from link_manager import links
 from selenium.common.exceptions import WebDriverException
 
-def main():
-    # Install chrome driver in current directory
-    os.environ['WDM_LOCAL'] = '1'
+def main(sb=True, tab=True):
     driver_path = ChromeDriverManager().install()
 
     for link in links.values():
         try:
             pin_odds = convert_odds(get_pin_odds(driver_path, link['pin']))
-            tab_odds = get_tab_odds(driver_path, link['tab'])
-            sb_odds = get_sb_odds(driver_path, link['sb'])
+            if tab:
+                tab_odds = get_tab_odds(driver_path, link['tab'])
+            if sb:
+                sb_odds = get_sb_odds(driver_path, link['sb'])
         except WebDriverException:
             print(f'Error scraping {link}')
             continue
         print(get_pos_ev(pin_odds, tab_odds), link['tab'])
         print(get_pos_ev(pin_odds, sb_odds), link['sb'])
 
+# Option to run main without tab
+def not_tab():
+    main(tab=False)
+
 if __name__ == '__main__':
-    main() 
+    main()
